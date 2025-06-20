072313180c4a47120b1fa7786abd32b8cf16f105
Thai Pham <thbinhkhanh@gmail.com>
2025-06-20 18:28:22 +0700

    Table

--- src/ChotSoLieu.jsx
+++ src/ChotSoLieu.jsx
@@ -104,7 +104,7 @@ function SummaryRow({ row, openGroups, setOpenGroups, summaryData }) {
           }
         }}
       >
-        <TableCell sx={{ fontWeight: "bold", textAlign: "center" }}>
+        <TableCell sx={{ fontWeight: "bold", textAlign: "left", pl: 2 }}>
           {isGroup && !isTruong && (
             <IconButton
               size="small"
@@ -125,7 +125,7 @@ function SummaryRow({ row, openGroups, setOpenGroups, summaryData }) {
       {isGroup && isOpen &&
         subRows.map((subRow, i) => (
           <TableRow key={i} sx={{ backgroundColor: "#f9fbe7", "&:hover": { backgroundColor: "#f0f4c3" } }}>
-            <TableCell sx={{ pl: 6, textAlign: "center" }}>{subRow.group}</TableCell>
+            <TableCell sx={{ pl: 1, textAlign: "center" }}>{subRow.group}</TableCell>
             <TableCell align="center">{subRow.siSo}</TableCell>
             <TableCell align="center">{subRow.anBanTru}</TableCell>
           </TableRow>
