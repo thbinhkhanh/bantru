1e02a6791ad6a73264aa55cfb906cb5eccd29c4d
Thai Pham <thbinhkhanh@gmail.com>
2025-06-20 10:59:58 +0700

    About

--- src/AdminLogin.jsx
+++ src/AdminLogin.jsx
@@ -82,7 +82,7 @@ export default function AdminLogin({ onCancel }) {
   };
 
   return (
-    <Box maxWidth={450} mx="auto" mt={-2}>
+    <Box maxWidth={450} mx="auto" mt={2}>
       <Card elevation={10} sx={{ p: 4, borderRadius: 4 }}>
         <Stack spacing={3}>
           <Box textAlign="center" sx={{ mb: 2 }}>
